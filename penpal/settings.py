a25c0d85e834354d55348f5ca527378348f6662e
Jim Ladd <j@portladds.com>
2024-09-23 15:12:20 -0700

    Add ability to create one-time pad (#6)

--- penpal/settings.py
+++ penpal/settings.py
@@ -1,1 +1,6 @@
-MAX_BLOCK_SIZE = 10**6  # in bytes
+# To prevent block creation from taking more than a second or two
+# set a maximum block size measured in bytes
+MAX_BLOCK_SIZE = 2**20
+
+# By default, blocks will be `STD_BLOCK_SIZE` bytes long
+STD_BLOCK_SIZE = 2**12
